91a2caf32db4677ed17d246bd983233f12da6ce3
byunsh96 <152119793+byunsh96@users.noreply.github.com>
2026-01-20 17:44:23 +0900

    [fix] Web plugin 파일 제거

--- super_native_extensions/lib/fork_super_native_extensions_web.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
-
-/// A web implementation of the ForkSuperNativeExtensionsPlatform of the ForkSuperNativeExtensions plugin.
-class SuperNativeExtensionsWeb {
-  /// Constructs a ForkSuperNativeExtensionsWeb
-  SuperNativeExtensionsWeb();
-
-  static void registerWith(Registrar registrar) {}
-}
